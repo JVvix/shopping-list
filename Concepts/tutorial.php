e67ed61de4e7ca6dadd226f559a82f027f8bcb83
fayedotly@gmail.com <fayedotly@gmail.com>
2022-06-13 20:58:57 -0700

    started php tutorial

---
+++ Concepts/tutorial.php
@@ -0,0 +1,15 @@
+<?php
+// script goes here
+
+// echo "Hello & Welcome to Simplylilearn";
+
+$str = "Hello and Welcome";
+$num = 9999;
+$float = 9.99;
+echo "<h1>" . $str . "</h1>";
+echo "<h2>" . $num . "</h2>";
+echo "<h3>" . $float . "</h3>";
+
+$laptop_brand = array("HP", "Lenovo", "Asus", "Samsung");
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> My First PHP Webpage! </h2>
+ 
+<?php
+// This is a single-line comment
+echo "Hello World.";
+# This is also a single-line comment
+?>
+ 
+</body>
+</html>
+
