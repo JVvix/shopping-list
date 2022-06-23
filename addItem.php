a7fd5a510fff6dd229a770281507592e247b0dee
fayedotly@gmail.com <fayedotly@gmail.com>
2022-06-23 12:14:31 -0700

    php header issue fixed again

--- addItem.php
+++ addItem.php
@@ -8,6 +8,20 @@ $dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
 	$conn= mysqli_connect($hostname, $username, $password, $db) or die("Connection failed: ".mysqli_connect_error());
+
+	$query = mysqli_query($dbconnect, "SELECT * FROM items")
+		or die (mysqli_error($dbconnect));
+
+	while ($row = mysqli_fetch_array($query)) {
+		if(isset($_POST{$row['item']})) {
+			$sql = "DELETE FROM items WHERE item =$item";
+			echo $sql;
+			$query = mysqli_query($conn, $sql);
+			if ($query) {
+				echo 'it works';
+			}
+		} 
+	}
 	if(isset($_POST['item'])) {
 		$item = $_POST['item'];
 
@@ -17,9 +31,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
 		$query = mysqli_query($conn, $sql);
 		if ($query) {
 			header("Location: http://pi0");
-			echo "<meta http-equiv='refresh' content='0'>";
 			// echo "<meta http-equiv='refresh' content='0'>";
-		// echo "it works!!"; 
+			// echo "<meta http-equiv='refresh' content='0'>";
+			echo "it works!!"; 
 
 		} else {
 			echo 'Error Occured';
--- index.php
+++ index.php
@@ -6,21 +6,22 @@ $hostname = "localhost";
 $username = "shop";
 $password = "ff";
 $db = "items";
+$pageRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) &&($_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0' ||  $_SERVER['HTTP_CACHE_CONTROL'] == 'no-cache'); 
 
 $dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
 $query = mysqli_query($dbconnect, "SELECT * FROM items")
-or die (mysqli_error($dbconnect));
+	or die (mysqli_error($dbconnect));
 
 while ($row = mysqli_fetch_array($query)) {
 	echo "<label>
-		<input type=checkbox value={$row['item']}>
+		<input type=checkbox name={$row['item']}>
 		<span>{$row['item']}</span>
 		</label>\n <br>";
 }
 
 ?>
 <form action="addItem.php" method="POST"> 
-	<label for=item>add item: </label><input type="text" name="text" id="name" required><br>
+	<label for=item>add item: </label><input type="text" name="item" id="item" required><br>
 	<input type="submit" name="submit">
 </form> 
